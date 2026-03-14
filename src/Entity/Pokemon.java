2543e1375127a220478b9e8884d58680d48a5e94
lez029 <lez029@ucsd.edu>
2026-03-13 21:27:55 -0700

    docs(Pokemon): add many detailed comments and TODO lists
    - Added documentation for most data fields, all constructors, and most methods in Pokemon class
    - Added TODO lists for future improvements and features

--- src/Entity/Pokemon.java
+++ src/Entity/Pokemon.java
@@ -11,8 +11,19 @@ import java.util.Arrays;
 import java.util.Random;
 import java.util.stream.Collectors;
 
+/**
+ * Represents a Pokemon instance in the game
+ * A Pokemon contains base data such as its pokemon species data
+ * and other instance data like identity values and effort values
+ *
+ * @author Lele Zhang
+ * @version 1.0
+ */
 public class Pokemon implements Cloneable{
     //Global Variables
+    /**
+     * Pokemon species data of this Pokemon
+     */
     public SpeciesData data;
     private String nickname;
     private int maxHP;
@@ -30,15 +41,36 @@ public class Pokemon implements Cloneable{
     private int[] ev = new int[6];
     private int[] iv = new int[6];
     private Item itemNow;
+    /**
+     * List of moves the Pokemon currently can use (max 4)
+     */
     private ArrayList<Move> moveListNow = new ArrayList<>(4);
+    /**
+     * List of moves that are available for the Pokemon (no max or min)
+     */
     private ArrayList<Move> moveListAvailable = new ArrayList<>();
+    /**
+     * Status now of the Pokemon
+     */
     private Status status;
+    // TODO: implement Gender class and finalize gender variable
     private Gender gender;
     private Ability ability;
     private Player owner;
+    // TODO: Add int friendness data field
+    // As some Pokemons may evolve based on their friendness
 
     // Constants
     private static final Random random = new Random();
+    /**
+     * Index for each stat out of a battle
+     * 0 for Health Point
+     * 1 for Attack
+     * 2 for Defense
+     * 3 for Special Attack
+     * 4 for Special Defense
+     * 5 for Speed
+     */
     private static final int INDEX_HP = 0;
     private static final int INDEX_ATTACK = 1;
     private static final int INDEX_DEFENSE = 2;
@@ -46,6 +78,15 @@ public class Pokemon implements Cloneable{
     private static final int INDEX_SD = 4;
     private static final int INDEX_SPEED = 5;
 
+    // Default Constructor
+
+    /**
+     * Create a Pokemon with　given species data and level
+     * (Mostly WILD Pokemon).
+     *
+     * @param data the SpeciesData of the Pokemon
+     * @param level the initial level of the Pokemon
+     */
     public Pokemon(SpeciesData data, int level) {
         this.data = data;
         this.nickname = data.speciesName();
@@ -57,6 +98,16 @@ public class Pokemon implements Cloneable{
         generateAbility(data.abilityList());
     }
 
+    /**
+     * Create a Pokemon with given species data and level for a specific player.
+     * For example, generate a new Pokemon for a NPC to battle with the player.
+     * ALso, it can be used to create a new Pokemon for a new Player
+     * in the game.
+     *
+     * @param data the SpeciesData of the Pokemon
+     * @param level the level of the Pokemon
+     * @param owner the owner of the Pokemon (NPC, Trainer, or Player)
+     */
     public Pokemon(SpeciesData data, int level, Player owner) {
         this.data = data;
         this.nickname = data.speciesName();
@@ -75,8 +126,14 @@ public class Pokemon implements Cloneable{
             ownerType = OwnerType.OTHER;
     }
 
-    // Deep Copy the input pokemon to the output pokemon
-    // after successfully catching or trading
+    /**
+     * Deep copy the input pokemon to the output pokemon after successfully
+     * catching or trading and create a new Pokemon with all data from original
+     * Pokemon as input with the call of copyFrom(Pokemon other).
+     *
+     * @param pokemon the original Pokemon (Caught or Traded)
+     * @param owner the owner of the Pokemon caught or traded
+     */
     public Pokemon(Pokemon pokemon, Player owner) {
         Pokemon copy = pokemon.clone();
         copy.owner = owner;
@@ -84,6 +141,12 @@ public class Pokemon implements Cloneable{
         copyFrom(copy);
     }
 
+    /**
+     * Deep copy this Pokemon and assign all data to a new Pokemon instance.
+     *
+     * @return a new Pokemon instance copied deeply to avoid effects on the
+     * original one.
+     */
     @Override
     public Pokemon clone() {
         try {
@@ -106,6 +169,13 @@ public class Pokemon implements Cloneable{
         }
     }
 
+    /**
+     * Shallow copy a Pokemon instance with all its reference variables and
+     * primitive variables (as we will do deep copy first with the call
+     * of clone()).
+     *
+     * @param other
+     */
     private void copyFrom(Pokemon other) {
         this.data = other.data;
         this.nickname = other.nickname;
@@ -132,15 +202,34 @@ public class Pokemon implements Cloneable{
         this.ability = other.ability;
     }
 
+    /**
+     * Set iv[] to initial random value (0~31) for each stat.
+     * Generate a random nature for the Pokemon instance and make it final.
+     */
     private void initRandomAttributes() {
         for (int i = 0; i < iv.length; i++) iv[i] = random.nextInt(32);
         this.nature = Nature.values()[random.nextInt(Nature.values().length)];
     }
 
+    /**
+     * Generate the ability of the Pokemon instance from its speices available
+     * ability list (ALways max 3 for a Pokemon).
+     *
+     * @param list an array list of all available abilities of the Pokemon
+     */
     public void generateAbility(ArrayList<Ability> list) {
         ability = list.get(random.nextInt(list.size()));
     }
 
+    /**
+     * Calculate the max health points of the Pokemon based on its species base
+     * stats, identity value, effort value, and level.
+     * Formula: (2 * BaseHP + ivHP + evHP / 4) * level /100 + level + 10
+     * (Ceil down)
+     * Generation: III and after III
+     *
+     * @return the max health points calculated based on data above
+     */
     public int calculateMaxHP() {
         return (2 * data.baseHP()
                 + iv[INDEX_HP]
@@ -148,6 +237,19 @@ public class Pokemon implements Cloneable{
                 * level / 100 + level + 10;
     }
 
+    /**
+     * Calculate other current stats of a Pokemon after its initialization,
+     * levelUp, or evolve based on its species base stats, identity value,
+     * effort value, and level
+     * Formula: (((2 * BaseVal + iv[baseVal] + ev[baseVal] / 4) * level / 100
+     * + 5) * natureModifier) (Ceil down)
+     * Generation: III and after III
+     *
+     * @param index index of each stat (0=HP, 1=Attack, 2=Defense,
+     *              3=Special Attack, 4=Special Defense, 5=Speed)
+     * @param baseVal the number of each base stat got from the SpeciesData
+     * @return integar calculation result of each stat
+     */
     public int calculateOtherStats(int index, int baseVal) {
         return (int)(
                 ((double) (2 * baseVal
@@ -155,6 +257,9 @@ public class Pokemon implements Cloneable{
                         * level / 100 + 5) * nature.getModifier()[index]);
     }
 
+    /**
+     * 
+     */
     public void setAllStats() {
         int oldMaxHP = maxHP;
         maxHP = calculateMaxHP();
@@ -175,6 +280,7 @@ public class Pokemon implements Cloneable{
         levelUP();
     }
 
+    // TODO: Implement levelUP method (call evolve() if isEvolve is true)
     public void levelUP() {
         while (expNow >= expNeed && level < 100) {
             expNow -= expNeed;
@@ -204,6 +310,7 @@ public class Pokemon implements Cloneable{
         return false;
     }
 
+    // TODO: Implement takeDamage(int damage) method
     public void takeDamage(int damage) {
         currentHP -= Math.min(damage, currentHP);
         if (isFainted()) {
@@ -214,4 +321,7 @@ public class Pokemon implements Cloneable{
     public boolean isFainted() {
         return currentHP <= 0;
     }
+
+    // TODO: Implement evolve() method
+    // TODO: Add public final boolean isEvolve data field to SpeciesData class
 }
